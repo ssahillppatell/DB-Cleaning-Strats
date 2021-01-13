73a070f6615d08334cc43b604f546bac1ed8b76f
Sahil Patel <ssahillppatell@gmail.com>
2021-01-13 14:30:14 +0530

    env file added

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 - Dataset and ERD taken from [here](https://www.postgresqltutorial.com/postgresql-sample-database/)
 
 #### Dependencies:
-pipenv: `pip install pipenv`
-postgresql: [Install](https://www.postgresql.org/) 
+- pipenv: `pip install pipenv`
+- postgresql: [Install](https://www.postgresql.org/) 
 
 #### Get Started:
+Make a `.env` file like `env.example`
+
 ```
 pipenv shell
 pipenv install
--- connection.py
+++ connection.py
@@ -1,9 +1,18 @@
+import os
 import psycopg2
 import psycopg2.extras
 from psycopg2 import Error
+from dotenv import load_dotenv
+
+load_dotenv()
+dbUser = os.environ.get('DB_USER')
+dbPass = os.environ.get('DB_PASS')
+dbHost = os.environ.get('DB_HOST')
+dbPort = os.environ.get('DB_PORT')
+dbName = os.environ.get('DB_NAME')
 
 try:
-	conn = psycopg2.connect(user="postgres", password="Sahil@123", host="127.0.0.1", port="5432", database="dvdrental")
+	conn = psycopg2.connect(user=dbUser, password=dbPass, host=dbHost, port=dbPort, database=dbName)
 
 	with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
 		cur.execute("SELECT name FROM category;")
