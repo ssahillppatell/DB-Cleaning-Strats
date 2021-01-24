7f0e41708a5eb2d49861760a007d90172ca1e4c4
Sahil Patel <ssahillppatell@gmail.com>
2021-01-24 22:32:23 +0530

    tkinter added

--- README.md
+++ README.md
@@ -2,13 +2,18 @@
 - CS322 Project
 - Tech Stack: Python, Postgresql
 - Dataset and ERD taken from [here](https://www.postgresqltutorial.com/postgresql-sample-database/)
+- UI: [Figma](https://www.figma.com/proto/nHYYsAzQl6GTb1C7DBhiGs/DB-Cleaning-Strats?node-id=1%3A2&scaling=min-zoom)
 
 #### Dependencies:
+- python: [Install](https://www.python.org/)
 - pipenv: `pip install pipenv`
 - postgresql: [Install](https://www.postgresql.org/) 
 
 #### Get Started:
-Make a `.env` file like `env.example`
+Make a `.env` file like `.env.example`
+Import Database:
+- Linux: `pg_restore -c -i -U <username> -d dvdrental -v "/location/to/dvdrental.tar" -W`
+- Windows: Import via pgadmin
 
 ```
 pipenv shell
--- connection.py
+++ connection.py
@@ -1,4 +1,6 @@
 import os
+import tkinter as tk
+
 import psycopg2
 import psycopg2.extras
 from psycopg2 import Error
@@ -11,36 +13,36 @@ dbHost = os.environ.get('DB_HOST')
 dbPort = os.environ.get('DB_PORT')
 dbName = os.environ.get('DB_NAME')
 
-try:
-	conn = psycopg2.connect(user=dbUser, password=dbPass, host=dbHost, port=dbPort, database=dbName)
+def checkoutOne(id):
+	try:
+		conn = psycopg2.connect(user=dbUser, password=dbPass, host=dbHost, port=dbPort, database=dbName)
 
-	with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-		cur.execute(" \
-			WITH t1 AS (Select *, DATE_PART('day', return_date - rental_date) AS date_difference FROM rental), \
-			t2 AS (SELECT rental_duration, date_difference, \
-						CASE \
-							WHEN rental_duration > date_difference THEN 'Returned early' \
-							WHEN rental_duration = date_difference THEN 'Returned on Time' \
-							ELSE 'Returned late' \
-						END AS Return_Status \
-					FROM film f \
-					JOIN inventory i \
-					USING(film_id) \
-					JOIN t1 \
-					USING (inventory_id)) \
-			SELECT Return_status, count(*) AS total_no_of_films \
-			FROM t2 \
-			GROUP BY 1 \
-			ORDER BY 2 DESC; \
-		")
-		print(cur.fetchall())
-	
-	conn.commit()
+		with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+			cur.execute(f" \
+				SELECT * FROM rental \
+				WHERE rental_id = {id} \
+			")
+			print(cur.fetchall())
+		
+		conn.commit()
 
-except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL", error)
+	except (Exception, Error) as error:
+		print("Error while connecting to PostgreSQL", error)
 
-finally:
-    if (conn):
-        conn.close()
-        print("PostgreSQL connection is closed")
+	finally:
+		if (conn):
+			conn.close()
+			print("PostgreSQL connection is closed")
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width = 400, height = 300)
+canvas.grid(columnspan = 3, rowspan = 3)
+
+rentalId = tk.Entry(root)
+rentalId.grid(column = 1, row = 0)
+
+actionBtn = tk.Button(root, text="Transact", command = lambda:checkoutOne(rentalId.get()))
+actionBtn.grid(column = 1, row = 1)
+
+root.mainloop()
